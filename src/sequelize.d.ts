663d866272f49cb64c9f80df9e5fee60833197db
Nico Gallinal <nico@rematter.com>
2023-05-23 12:48:12 +0200

    fix include as syntax typing in virtual constructor

--- src/sequelize.d.ts
+++ src/sequelize.d.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line unused-imports/no-unused-imports
 import * as Sequelize from "sequelize";
+import { VirtualDataType } from "sequelize";
 
 declare module "sequelize" {
   export interface ModelAttributeColumnOptions<M extends Model = Model>
@@ -8,7 +9,6 @@ declare module "sequelize" {
   }
 
   export type IncludeAsCallback = (includeAs: string) => [Literal | Fn, string];
-
   /**
    * Adds type-safety support to our extension for 'includeAs' in 'virtual columns'.
    */
@@ -23,7 +23,6 @@ declare module "sequelize" {
       fields?: string[] | IncludeAsCallback
     ): VirtualDataType<T>;
   }
-
   export abstract class Association<
     S extends Model = Model,
     T extends Model = Model
